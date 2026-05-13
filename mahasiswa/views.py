f074618a670cacec7791e89e5934c4031edde331
Najwan Ramadhan <najwanramadhan123@gmail.com>
2026-05-13 14:18:34 +0700

    first commit

---
+++ mahasiswa/views.py
@@ -0,0 +1,16 @@
+from urllib import request
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Mahasiswa
+
+def index(request):
+    context = {
+        'judul': 'Halo Mahasiswa',
+        'deskripsi': 'Contoh halaman index menggunakan Django templates dan static files.'
+    }
+    return render(request, 'mahasiswa/index.html', context)
+
+def daftar_mahasiswa(request):
+    mahasiswas = Mahasiswa.objects.all()
+    return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
