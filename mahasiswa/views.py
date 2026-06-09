8622825f98951358c07c3a0b525d894344016b55
Najwan Ramadhan <najwanramadhan123@gmail.com>
2026-06-09 21:48:32 +0700

    perbaruan sampai prak5

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,5 +1,5 @@
 from urllib import request
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Mahasiswa
@@ -11,6 +11,7 @@ def index(request):
     }
     return render(request, 'mahasiswa/index.html', context)
 
+@login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
     mahasiswas = Mahasiswa.objects.all()
     return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
