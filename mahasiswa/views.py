ded606446e7460a5519a36105749e29838b89112
Najwan Ramadhan <najwanramadhan123@gmail.com>
2026-06-10 13:29:14 +0700

    tambah fitur CRUD mahasiswa

--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,8 +1,7 @@
-from urllib import request
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mahasiswa
+
 
 def index(request):
     context = {
@@ -11,7 +10,52 @@ def index(request):
     }
     return render(request, 'mahasiswa/index.html', context)
 
+
 @login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
     mahasiswas = Mahasiswa.objects.all()
-    return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
+
+    return render(request, 'mahasiswa/daftar.html', {
+        'mahasiswas': mahasiswas
+    })
+
+
+@login_required(login_url='/accounts/login/')
+def tambah_mahasiswa(request):
+    if request.method == 'POST':
+        Mahasiswa.objects.create(
+            nim=request.POST['nim'],
+            nama=request.POST['nama'],
+            programstudi=request.POST['programstudi'],
+            angkatan=request.POST['angkatan']
+        )
+
+        return redirect('daftar_mahasiswa')
+
+    return render(request, 'mahasiswa/form.html')
+
+
+@login_required(login_url='/accounts/login/')
+def edit_mahasiswa(request, id):
+    mahasiswa = get_object_or_404(Mahasiswa, id=id)
+
+    if request.method == 'POST':
+        mahasiswa.nim = request.POST['nim']
+        mahasiswa.nama = request.POST['nama']
+        mahasiswa.programstudi = request.POST['programstudi']
+        mahasiswa.angkatan = request.POST['angkatan']
+        mahasiswa.save()
+
+        return redirect('daftar_mahasiswa')
+
+    return render(request, 'mahasiswa/form.html', {
+        'mahasiswa': mahasiswa
+    })
+
+
+@login_required(login_url='/accounts/login/')
+def hapus_mahasiswa(request, id):
+    mahasiswa = get_object_or_404(Mahasiswa, id=id)
+    mahasiswa.delete()
+
+    return redirect('daftar_mahasiswa')
